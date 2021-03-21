96c8cd072d7264cacc445ad21577c258fb19fafe
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-03-21 19:31:14 -0400

    commit tree 0382a2979baeab5b56e27a74723ad3aa694bae8f

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,18 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinatesButton extends React.Component {
+
+  onClick = (event) => {
+    const array = [event.clientX, event.clientY];
+    this.props.onReceiveCoordinates(array);
+
+  }
+
+  render() {
+    return <button onClick={this.onClick}>Coordinates</button>
+  }
+
+
+}
+
+export default CoordinatesButton;
